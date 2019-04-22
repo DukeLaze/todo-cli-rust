013c867ba4194b12821be47f5c6c39591a30c316
Lasse Seivaag <lasse_0407@hotmail.com>
2019-04-22 03:31:45 +0200

    #5 Basic implementation

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,83 @@
-use std::{vec, io, process};
+use std::{io, io::Write, process};
 
-struct TodoList{
-    entries : Vec<String>
+pub struct TodoElement{
+    pub id : u32,
+    pub text : String,
+    pub done : bool
+}
+
+impl TodoElement{
+    fn new(id : u32, text : String, done : bool) -> TodoElement {
+        TodoElement {
+            id,
+            text,
+            done
+        }
+    }
+}
+
+pub struct TodoList{
+    pub entries : Vec<TodoElement>,
+    pub index : u32
 }
 
 impl TodoList {
-    fn add(&mut self, element : String){
+    fn add(&mut self, element : TodoElement){
         self.entries.push(element);
     }
 
     fn list_all(&self) {
         for element in &self.entries {
-            println!("{}", element);
+            println!("Id: {} - Status: {}\nTask: {}", element.id, if element.done {"[Completed]"} else {"[Uncompleted]"}, element.text);
         }
+    }
+
+    fn update_text(&mut self, id : u32, text : String){
+        self.entries[id as usize].text = text;
     }
 }
 
 enum Commands{
     Help,
-    Add(String),
+    Add(TodoElement),
     ListAll,
+    UpdateText(u32, String),
+    Complete(u32),
     Exit
 }
 
 fn main() {
     let mut list = TodoList {
-        entries : Vec::<String>::new()
+        entries : Vec::<TodoElement>::new(),
+        index : 0
     };
 
-    println!("Super Simple To-Do-List CLI 0.1.0\n >> ");
+    println!("Super Simple To-Do-List CLI 0.1.0");
 
     loop{
+        print!(">>");
+        io::stdout().flush().unwrap();
         let mut input = String::new();
-        
         io::stdin().read_line(&mut input).expect("Unable to read line");
 
         let split_input : Vec<&str> = input.split_whitespace().collect();
 
+        if split_input.len() == 0 {
+            continue;
+        }
         let command = match split_input[0] {
             "list" => Commands::ListAll,
-            "add" => Commands::Add(split_input[1].to_string()),
+            "add" => {
+                let cmd = Commands::Add(TodoElement::new(list.index, split_input[1].to_string(), false));
+                list.index = list.index + 1;
+                cmd
+            },
+            "update" => {
+                Commands::UpdateText(split_input[1].parse::<u32>().unwrap(), split_input[2].to_string())
+            },
+            "complete" => {
+                Commands::Complete(split_input[1].parse::<u32>().unwrap())
+            }
             "exit" => Commands::Exit,
             _ => Commands::Help
         };
@@ -48,6 +86,8 @@ fn main() {
             Commands::ListAll => list.list_all(),
             Commands::Add(task) => list.add(task),
             Commands::Help => println!("Nope"),
+            Commands::UpdateText(id, text) => list.update_text(id, text),
+            Commands::Complete(id) => list.entries[id as usize].done = true,
             Commands::Exit => process::exit(0)
         }
         
